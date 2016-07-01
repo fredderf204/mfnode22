4636ac28bf735005e5e47300ea31206aca6cba54
Michael Friedrich <mifrie@microsoft.com>
2016-07-01 13:46:15 +1000

    remove app insights

--- server.js
+++ server.js
@@ -5,9 +5,6 @@ var path = require('path');         //define path
 var os = require('os');             //define os
 var appInsights = require("applicationinsights");
 
-//start app insights
-appInsights.setup("uvb8yimuz8uttbiwdd24fc6rv8tapboze5qgvqtc").start();
-
 //get ip addresses
   var interfaces = os.networkInterfaces();
   var addresses = [];
