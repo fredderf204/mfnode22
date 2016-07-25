8af1b994196ec83abbf2a4a14d4889e72d3cd986
Michael Friedrich <mifrie@microsoft.com>
2016-07-25 12:03:40 +1000

    changed IP address logic back

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.get('/', function (req, res) {
  var sitename = process.env.WEBSITE_HOSTNAME;
  var region = process.env.REGION_NAME;
  var websku = process.env.WEBSITE_SKU;
- var localaddr = process.env.LOCAL_ADDR;
+ var scmtype = process.env.ScmType;
 
   res.render('index', { //ejs always looks in the /views folder
     hostname: hostname,
@@ -41,7 +41,7 @@ app.get('/', function (req, res) {
     sitename: sitename,
     region: region,
     websku: websku,
-    localaddr: localaddr
+    scmtype: scmtype
     });
   });
 
