fc75c3d97a943585d64a6dd87851e368c76eb13d
Michael Friedrich <mifrie@microsoft.com>
2016-11-17 10:29:28 +1100

    changed unit test

--- test/test.js
+++ test/test.js
@@ -1,5 +1,4 @@
 var expect = require('chai').expect;
-var app = require('../server.js');
 var addresses = require('../server.js');
 
 describe('unittest', function() {
@@ -7,11 +6,5 @@ describe('unittest', function() {
     it('expect addreses to be an array', function() {
       expect(addresses).to.be.a('array');
     }); 
-    }); 
-  describe('hostname', function() {
-    it('expect hostname to be an array', function() {
-      
-      expect().to.be.a('string');
-  });
 });
 });
